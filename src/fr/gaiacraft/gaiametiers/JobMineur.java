7c862cf5fc90677ca2c0cbf878b619b5cbd7c05a
Eclipse2212 <lucas.assier33@gmail.com>
2018-02-03 22:04:50 +0100

    Began Mining handlers, needs for moar doc

--- src/fr/gaiacraft/gaiametiers/JobMineur.java
+++ src/fr/gaiacraft/gaiametiers/JobMineur.java
@@ -1,13 +1,22 @@
 package fr.gaiacraft.gaiametiers;
 
 import org.bukkit.Material;
+import org.bukkit.block.BlockState;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
 public class JobMineur implements Listener {
 
-    public void onItemBroken(BlockBreakEvent ev) {
+    public void onBlockBroken(BlockBreakEvent ev) {
         Material mat = ev.getBlock().getType();
+        BlockState st8 = ev.getBlock().getState();
+
+        /* TODO: regarder pour la gestion andesite granite diorite */
+
+        if(mat == Material.STONE) {
+            //Do something
+        }
     }
 }
 
